1c31bbd5b56bbba74ab5e9335a388492bbc8edb3
unknown <harisnowpop@gmail.com>
2022-08-19 08:53:53 +0700

    day7

--- index.js
+++ index.js
@@ -4,10 +4,50 @@ const app = express()
 
 const port = 3000
 
-app.get('/index.html', function(request,response){
-    response.send('/index.html')
-})
+const isLogin = true
 
-app.listen(port, () => {
-    console.log(`Personal Web App listening on port ${port}`)
-})
+app.set('view engine', 'hbs')
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));  //kasih tau bisa nerima data karena default true
+
+
+app.get("/", (req, res) => {
+    res.render('index',{ isLogin });
+  });
+  
+  app.get('/project', (req, res) => {
+    res.render('project');
+  });
+  
+  app.post('/project', (req, res) => {
+    console.log(req.body);
+  
+    res.redirect('/');
+  });
+  
+  app.get('/contact', (req, res) => {
+    res.render('contact');
+  });
+  
+  app.get('/project-details/:id', (req, res) => {
+    const { id } = req.params
+    console.log(id)
+    res.render('project-details')
+  })
+
+  // app.get("/project-details/:id", function (req, res) {
+  //   let id = req.params.id;
+  //   res.render("project-details", {
+  //     project: {
+  //       id,
+  //       title: "Pelatihan Bootcamp Dumbways",
+  //       content: "lore lore lore RIP love wkwk",
+  //     },
+  //   });
+  // });
+
+
+  app.listen(port, () => console.log(`Server running on port ${port}`))
+
+// https://discord.gg/9g2ar9nP
